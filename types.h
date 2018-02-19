dba934200048c842e994d95263acac7f2d460114
jose <jose@libelula.joslumar-int>
2018-02-19 14:13:49 +0100

    Cosmetic source changes

--- types.h
+++ types.h
@@ -1,2 +1,5 @@
+#ifndef DSLRTEMP_types_h
+#define DSLRTEMP_types_h
 enum lcd_mode_type { temp_coldfinger, temp_hotside, temp_coldside, temp_cam, pwm_peltier, heat_heater, setup_temp, setup_heater, setup_save, alarm};
 enum logprio_type { log_error, log_warn, log_info, log_debug};
+#endif
